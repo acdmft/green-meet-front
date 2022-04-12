208a5bcd9e2fb6d2d5ea532c489233235b2627da
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-12 11:48:10 +0200

    Create content of ContactForm.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ActionCard from "./components/ActionCard";
+import ContactForm from "./components/ContactForm";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+import ContactAdmin from "./views/ContactAdmin";
 
 // Views
 import Homepage from "./views/Homepage";
@@ -11,6 +13,7 @@ function App() {
     <div>
       <Homepage />
       <ActionCard />
+      <ContactForm />
     </div>
   );
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
+import { useForm } from "react-hook-form";
+
 function ContactForm() {
-  return <div>ContactForm</div>;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="title">Titre</label>
+        <input
+          name="title"
+          {...register("title", {
+            required: true,
+            maxLength: 50,
+          })}
+        />
+        <label htmlFor="message">Message</label>
+
+        <input
+          type="text"
+          name="message"
+          {...register("message", {
+            required: true,
+            minLength: 10,
+            maxLength: 500,
+          })}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default ContactForm;
