ca6a42c5f08dcc55df6bbb3102ad73ccb871feb9
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-12 10:50:23 +0200

    Add some content to ActionDetails.jsx

--- src/views/ActionDetails.jsx
+++ src/views/ActionDetails.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
 
 function ActionDetails() {
-  return <div>ActionDetails</div>;
+  return (
+    <div>
+      <h2>Titre action (Nettoyage parc,...)</h2>
+      <img></img>
+      <h4>Organisateur : </h4>
+      <p>PROPS</p>
+      <h4>Date : </h4>
+      <p>PROPS</p>
+      <h4>Horaires : </h4>
+      <p>PROPS</p>
+      <h4>Détails : </h4>
+      <p>
+        PROPS Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam
+        perspiciatis cumque, cum ea esse minus voluptate adipisci excepturi
+        deserunt quos nemo dolor quis iusto doloribus cupiditate eius fugit
+        itaque. Culpa.
+      </p>
+      <h4>Contacter l'organisateur : </h4>
+      <ContactForm />
+    </div>
+  );
 }
 
 export default ActionDetails;
