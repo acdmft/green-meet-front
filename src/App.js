d569d3ba831b4336865e73602fbd4b9c58d9e47c
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-12 10:41:10 +0200

    Add Homepage views with some content and import in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+// Views
+import Homepage from "./views/Homepage";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/views/Homepage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Green Meet, les rencontres écolos</h1>
+      <img></img>
+      <h3>Qu'est-ce que Green Meet ?</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis illo,
+        porro id voluptatum illum est dignissimos exercitationem. Expedita, quas
+        pariatur! Aliquid ducimus commodi ipsam. Culpa minima quia perferendis
+        minus sed dolorum dolores! Sint exercitationem reprehenderit in cum
+        quisquam accusamus quam, quibusdam molestiae doloremque repudiandae.
+        Libero autem minus expedita cupiditate veniam quo dolore est consectetur
+        vero. Nobis nemo et tempore, voluptates laboriosam tenetur doloribus est
+        iusto cum quisquam, expedita minus maxime adipisci debitis aliquam
+        beatae alias. Nihil obcaecati, voluptatibus velit dolor iusto totam
+        error est, voluptate saepe cupiditate distinctio cum praesentium
+        perferendis. Odio eos itaque ratione vitae laudantium obcaecati ad
+        voluptatibus sequi, nulla iste ullam praesentium. Corporis, laudantium
+        repellendus culpa et natus exercitationem quas possimus aliquid eveniet
+        reiciendis quo ab eos voluptas enim, dicta maxime obcaecati quia vitae
+        optio sint beatae rem? Labore dolorem itaque adipisci assumenda officia
+        tempore voluptas eius!
+      </p>
+    </div>
+  );
+}
+
+export default Homepage;
