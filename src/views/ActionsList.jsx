de1cbab7df5b4f95912ed336cc9b539b81d4469a
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-14 16:08:00 +0200

    Improve style of ActionCard component

--- src/views/ActionsList.jsx
+++ src/views/ActionsList.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ActionCard from "../components/ActionCard";
 
 function ActionsList() {
-  return <div>ActionsList</div>;
+  const [actions, setActions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((res) => {
+        setActions(res);
+      });
+  }, []);
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        {actions.slice(0, 12).map((action) => {
+          return (
+            <ActionCard
+              key={action.name}
+              title={action.name.common}
+              description={action.altSpellings}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ActionsList;
