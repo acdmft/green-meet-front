74164e5791fcafd104137a53479baa29ebbcbb71
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-14 11:54:16 +0200

    Improve loginForm with error messages

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,30 +1,64 @@
-import React from "react";
+import { React, useState } from "react";
 import { useForm } from "react-hook-form";
 
 function LoginForm() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   const onSubmit = (data) => console.log(data);
+
+  // Si la checkbox est activée, on garde le email dans le local storage :
+  // if (rememberMe === true) {
+  //   localStorage.setItem("email", data.email);
+  // }
+
+  // State remember pour rappel email et password utilisateur
+  const [rememberMe, setRememberMe] = useState(false);
+
+  // Fonction pour changement state remember :
+  const handleChange = (e) => {
+    setRememberMe(e.target.checked);
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">Email</label>
         <input
+          {...register("email", { required: true, maxLength: 150 })}
+          type="text"
           name="email"
-          {...register("email", {
-            required: true,
-            maxLength: 150,
-            pattern: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/i,
-          })}
+          id="email"
+          placeholder="email"
+          // value={localStorage.getItem("email")}
         />
-        <label htmlFor="password">Password</label>
-
+        {/* Message d'erreur si input invalide : */}
+        {errors.email && <span>Please enter a valid email</span>}
         <input
+          {...register("password", { required: true, maxLength: 6 })}
           type="password"
           name="password"
-          {...register("password", { required: true, minLength: 6 })}
+          id="password"
+          placeholder="Password"
         />
-        <input type="submit" />
+        {/* Message d'erreur si input invalide : */}
+        {errors.password && <span>Please enter a valid password</span>}
+        {/* <div className="container">
+          <input
+            {...register("checkbox")}
+            type="checkbox"
+            name="remember"
+            value="remember"
+            onChange={handleChange}
+          />
+          <label for="remember" style={{ color: "white" }}>
+            Remember me
+          </label>
+        </div> */}
+
+        <button type="submit">Login</button>
       </form>
     </div>
   );
