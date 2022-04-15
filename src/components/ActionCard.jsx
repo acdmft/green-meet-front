06070e35dd3c13a860b2b44572d98742f254108b
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-15 10:57:25 +0200

    Trying render conditionnal in ActionsList.jsx

--- src/components/ActionCard.jsx
+++ src/components/ActionCard.jsx
@@ -9,7 +9,7 @@ function ActionCard(props) {
   };
   return (
     <div>
-      <div class="p-6 h-64 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-5 flex flex-col">
+      <div class="p-6 h-64 bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 m-5 flex flex-col">
         <a href="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {props.title}
--- src/views/ActionsList.jsx
+++ src/views/ActionsList.jsx
@@ -1,8 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+// Components
 import ActionCard from "../components/ActionCard";
+import Button from "../components/Button";
 
 function ActionsList() {
   const [actions, setActions] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [filteredActions, setFilteredActions] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    setUserInput(data.city);
+  };
+
+  useEffect(() => {
+    if (userInput !== "") {
+      fetch(`https://restcountries.com/v3.1/region/${userInput}`)
+        .then((res) => res.json())
+        .then((res) => {
+          setFilteredActions(res);
+        });
+    }
+  }, [userInput]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -11,18 +37,72 @@ function ActionsList() {
         setActions(res);
       });
   }, []);
+
+  function RenderActions() {
+    actions.slice(0, 12).map((action) => {
+      return (
+        <ActionCard
+          key={action.name}
+          title={action.name.common}
+          description={action.altSpellings}
+        />
+      );
+    });
+  }
+
+  function RenderFilteredActions() {
+    filteredActions.map((action) => {
+      return (
+        <ActionCard
+          key={action.name}
+          title={action.name}
+          description={action.altSpellings}
+        />
+      );
+    });
+  }
+
+  console.log(actions);
+  //
+
   return (
-    <div className="flex justify-center">
-      <div className="grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-        {actions.slice(0, 12).map((action) => {
-          return (
-            <ActionCard
-              key={action.name}
-              title={action.name.common}
-              description={action.altSpellings}
+    <div className="flex flex-col justify-center">
+      <div>
+        <form
+          className="flex flex-row mx-auto items-center w-1/2 space-x-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col w-full">
+            {/* <label htmlFor="city">City</label> */}
+            <input
+              className="border-2 h-10"
+              {...register("city", {
+                required: true,
+                maxLength: 150,
+                minLength: 1,
+              })}
+              type="text"
+              name="city"
+              id="city"
+              placeholder="city"
+              // value={localStorage.getItem("email")}
             />
-          );
-        })}
+            {/* Message d'erreur si input invalide : */}
+            {errors.city && (
+              <span className="w-full text-red-600 italic text-xs">
+                Please enter a valid city
+              </span>
+            )}
+          </div>
+
+          <Button type="submit" className="w-full mt-6">
+            Rechercher
+          </Button>
+        </form>
+      </div>
+      <div className="grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        {/* {filteredActions === undefined ? ( */}
+        {actions.length !== 0 && RenderActions()}
       </div>
     </div>
   );
