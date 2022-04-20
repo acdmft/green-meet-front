64fc7f241a9f7d607630829dd9771585e42f1415
jlmoer <jlmoerdijk@gmail.com>
2022-04-20 09:50:09 +0200

    mentions légales terminées

--- src/views/Mentions.jsx
+++ src/views/Mentions.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import ArrowButton from "../components/ArrowButton";
 
-function Mentions() {
-  return <div>Mentions</div>;
+function Mentions(props) {
+  return (
+    <div>
+      <h1 className="text-gmgreen text-lg md:text-xl lg:text-3xl my-7 flex justify-center">Mentions Légales</h1>
+      <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 m-5 flex flex-col">
+        <h3 className="my-7 mx-12 text-gmgreen font-bold text-lg" id="contenu">1 - Édition du site</h3>
+        <p className="mx-12 text-justify">En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://www.green-meet.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
+
+        <p className="mx-12 text-justify">Propriétaire du site : Julie Desvaux - Contact : desvaux.julie@gmail.com 0698490766 - Adresse : 2 allée des chalands, 77200 Torcy.</p>
+
+        <p className="mx-12 text-justify">Hébergeur : 1&1 / IONOS - 7 Place de la Gare - 57200 Sarreguemines - Téléphone : 09.70.80.89.11</p>
+
+        <p className="mx-12 text-justify">Autres contributeurs : Jean-Léonard Moerdijk Caroline Lamy Andrei Zheksimbaev Adrien Gogois</p>
+
+        <h3 className="my-7 mx-12 text-gmgreen font-bold text-lg" id="contenu">2 - Propriété intellectuelle et contrefaçons.</h3>
+        <p className="mx-12 text-justify">Julie Desvaux est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.</p>
+
+        <p className="mx-12 text-justify">Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Julie Desvaux.</p>
+
+        <p className="mx-12 text-justify">Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+
+        <h3 className="my-7 mx-12 text-gmgreen font-bold text-lg" id="contenu">3 - Limitations de responsabilité.</h3>
+        <p className="mx-12 text-justify">Julie Desvaux ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://www.green-meet.fr.</p>
+
+        <p className="mx-12 text-justify">Julie Desvaux décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://www.green-meet.fr.</p>
+
+        <p className="mx-12 text-justify">Julie Desvaux s’engage à sécuriser au mieux le site https://www.green-meet.fr, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.</p>
+
+        <p className="mx-12 text-justify">Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Julie Desvaux se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.</p>
+
+        <p className="mx-12 text-justify">Le cas échéant, Julie Desvaux se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).</p>
+
+        <h3 className="my-7 mx-12 text-gmgreen font-bold text-lg" id="contenu">4 - CNIL et gestion des données personnelles.</h3>
+        <p className="mx-12 text-justify">Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre https://www.green-meet.fr/confidentialite.</p>
+
+        <h3 className="my-7 mx-12 text-gmgreen font-bold text-lg" id="contenu">5 - Liens hypertextes et cookies.</h3>
+        <p className="mx-12 text-justify">Le site https://www.green-meet.fr contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://www.green-meet.fr.</p>
+
+        <p className="mx-12 text-justify">La navigation sur le site https://www.green-meet.fr est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.</p>
+
+        <p className="mx-12 text-justify">Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.</p>
+
+        <p className="mx-12 text-justify">Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.</p>
+        <p className="mx-12 text-justify">Les cookies sont enregistrés pour une durée maximale de mois.</p>
+
+        <p className="mx-12 text-justify">Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre https://www.green-meet.fr/confidentialite. [Consignes : ajoutez ici le lien hypertexte vers votre politique de confidentialité ou vers votre politique de cookies si vous en avez une (c'est le cas si vous utilisez Complianz)]</p>
+        <h3 className="my-7 mx-12 text-gmgreen font-bold text-lg" id="contenu">6 - Droit applicable et attribution de juridiction.</h3>
+        <p className="mx-12 text-justify">Tout litige en relation avec l’utilisation du site https://www.green-meet.fr est soumis au droit français. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Meaux.</p>
+
+        {/* <a
+        href="#"
+        class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Voir plus
+        <svg
+          class="ml-2 -mr-1 w-4 h-4"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </a> */}
+      </div>
+    </div>
+  );
 }
 
 export default Mentions;
