47d1754f685f2fed66bef01c5826173dc42b1387
jlmoer <jlmoerdijk@gmail.com>
2022-04-22 17:01:40 +0200

    contributors cards style improved

--- src/components/ContributorsCard.jsx
+++ src/components/ContributorsCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 
 function ContributorsCard(props) {
     return (
 
-        <div className="hover:bg-gmvert-dark ease-in-out transform translate hover:-translate-y-1 hover:scale-105 hover:transition-all duration-500 p-6 h-64 rounded-lg border bg-gray-800 opacity-80 border-gray-700 m-5 flex flex-col relative md:w-1/3 flex flex-col items-center">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">{props.name}</h5>
+        <div className="hover:bg-gmvert-dark ease-in-out transform translate hover:-translate-y-1 hover:scale-105 hover:transition-all duration-500 p-6 h-64 rounded-lg border bg-gray-800 opacity-80 border-gray-700 m-5 flex flex-col items-center text-gmlime-light gap-5">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">{props.name}</h5>
+            <a className="text-2xl" target="_blank" href={`${props.linkGithub}`}><FaGithub /></a>
+            <a className="text-2xl" target="_blank" href={`${props.linkLinkedin}`}><FaLinkedin /></a>
         </div>
 
     )
